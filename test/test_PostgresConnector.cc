23c1a8dae8ec3fee194d84d607e403644d292f9d
yli5 <yli5@caltech.edu>
2016-05-05 16:48:46 -0700

    add PostgresConnector

---
+++ test/test_PostgresConnector.cc
@@ -0,0 +1,21 @@
+#include <TupleReader.h>
+#include <ColumnConfigParser.h>
+#include <PostgresConnector.h>
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+
+
+using namespac std;
+
+int main(){
+	PostgresConnector conn("darkphoton", "test");
+	conn.register();
+	conn.insert("mcLen",3);
+	conn.insert("mass", 0.4523);
+	conn.insert("mclund", "{1, 2, 3}");
+	conn.insert("mcd1mass", "{1.2, 3.4, 5.6}");
+	conn.exec();
+	return 0;
+}
