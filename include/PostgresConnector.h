d3e2c4431354cc9d0d97dbf895ea5051d145ab7a
jkim- <jaehongkim86@gmail.com>
2016-05-06 12:24:48 -0700

    Commit to pull from main repository.

--- include/PostgresConnector.h
+++ include/PostgresConnector.h
@@ -14,27 +14,33 @@ PostgresConnector(std::string db_name,
                   std::vector<std::string> var_names);
 ~PostgresConnector();
 
-void regis();
-void insert(std::string column, std::string value);
+//void regis();
+//void insert(std::string column, std::string value);
+void bind();
 void exec();
 
 private:
 	//connector
 	PGconn* conn_;
-	char* pghost_ = NULL;
-	char* pgport_ = NULL;
-	char* pgoptions_ = NULL;
-	char* pgtty_ = NULL;
+	char* pghost_ = nullptr;
+	char* pgport_ = nullptr;
+	char* pgoptions_ = nullptr;
+	char* pgtty_ = nullptr;
 	std::string db_name_;
-	char* login_ = NULL;
-	char* pwd_ = NULL;
+	char* login_ = nullptr;
+	char* pwd_ = nullptr;
 	std::string table_name_;
 
 	//command execution
 	std::string command_column_;
 	std::string command_value_;
-	PGresult* res_;
+	PGresult *res_;
+	PGresult *stmt_name_;
 
+  //variables
+  std::vector<std::string> var_names_;
+  size_t num_vars_;
+  const char *param_values_;
 };
 
 #endif
--- src/PostgresConnector.cc
+++ src/PostgresConnector.cc
@@ -8,34 +8,51 @@
 using std::string;
 using std::vector;
 
+static void 
+exit_nicely(PGconn *conn) {
+  PQfinish(conn);
+  exit(EXIT_FAILURE);
+}
+
 PostgresConnector::PostgresConnector(string db_name, 
                                      string table_name,
-                                     vector<string> var_names){
+                                     vector<string> var_names)
+    : var_names_(var_names), 
+      num_vars_(var_names.size()),
+      param_values_(new const char*[var_names.size()]("DEFAULT")) {
+
+  // Initialize and open connection to the databse
 	db_name_ = db_name;
 	table_name_ = table_name;
 	conn_ = PQsetdbLogin(pghost_, pgport_, pgoptions_, pgtty_, 
                        db_name_.c_str(), login_, pwd_);	
-	//res_ = PQexec(conn_, command_column_.c_str());
-	if (PQstatus(conn_) != CONNECTION_OK)
-		throw PQerrorMessage(conn_);
+  if (PQstatus(conn_) != CONNECTION_OK) {
+    cerr << "Connection to database failed: " << PQerrorMessage(conn_);
+    exit_nicely(conn_);
+  }
   
   // Construct command to be passed preparing the statement 
-  const char* stmtName = "PREPARE_DATA_FOR_INSERT";
+  const char* stmt_name_ = "PREPARE_DATA_FOR_INSERT";
   string stmt_command = "INSERT INTO" + table_name + " (";
-  for (const auto &n : var_names) {
+  for (const auto &n : var_names_) {
     stmt_command += n + ",";
   }
   stmt_command.pop_back();
   stmt_command += ") VALUES (";
-  for (size_t i = 1; i <= var_names.size(); ++i) {
+  for (size_t i = 1; i <= num_vars_; ++i) {
     stmt_command += "$" + std::to_string(i) + ",";
   }
   stmt_command.pop_back();
   stmt_command += ");";
-  PGresult* stmt = PQprepare(conn_, stmtName, stmt_command.c_str(), 
-                             var_names.size(), NULL);
-  std::cout << PQresultErrorMessage(stmt) << " Test to use stmt. "
-            << std::endl;
+
+  // Prepare the statement
+  res_ = PQprepare(conn_, stmt_name_, stmt_command.c_str(), 
+                   num_vars_, nullptr);
+  if (PQresultStatus(res_) != PGRES_COMMAND_OK) {
+    cerr << "Prepare failed: " << PQerrorMessage(res_);
+    PQclear(res_);
+    exit_nicely(conn_);
+  }
 }
 
 PostgresConnector::~PostgresConnector(){
@@ -43,14 +60,20 @@ PostgresConnector::~PostgresConnector(){
 	PQclear(res_);
 }
 
-void PostgresConnector::regis(){
-	command_column_ = "INSERT INTO " + table_name_ + " (";
-	command_value_ = " values (";
-}
+//void PostgresConnector::regis(){
+//	command_column_ = "INSERT INTO " + table_name_ + " (";
+//	command_value_ = " values (";
+//}
 
-void PostgresConnector::insert(std::string column, std::string value){
-	command_column_ += column + ",";
-	command_value_ += value + ",";
+//void PostgresConnector::insert(std::string column, std::string value){
+//	command_column_ += column + ",";
+//	command_value_ += value + ",";
+//}
+
+void PostgresConnector::bind() {
+  const char *param_values[num_vars_];
+  for (const auto &v : var_names_) {
+  }
 }
 
 void PostgresConnector::exec(){
